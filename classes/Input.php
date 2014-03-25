c2607b6f411444a940fc3255e3f574eb73dd41d3
Tanya Parker <tanyacparker@gmail.com>
2014-03-25 11:35:32 -0700

    empty Input class and tests

---
+++ classes/Input.php
@@ -0,0 +1,11 @@
+<?php
+
+class Input {
+
+	public function __construct() {
+
+	}
+
+}
+
+?>
---
+++ tests/InputTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../classes/Input.php';
+
+class InputTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+
+	}
+
+	public function tearDown() {
+		
+	}
+
+}
+
+?>
